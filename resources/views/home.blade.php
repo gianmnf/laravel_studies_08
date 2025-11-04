6a94dbd5ed275ba4fc257be01c5756c3d6e5406c
Gian Michel <gianmnf@gmail.com>
2025-11-04 14:26:32 -0300

    006

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,12 @@
             </div>
             <div class="col d-flex justify-content-end align-items-center gap-5 p-3">
                 <span>User: <strong class="text-info">[user]</strong></span>
-                <a href="#" class="btn btn-primary">Logout</a>
+                <form action="{{ route('logout') }}" method="post">
+                    @csrf
+                    <button type="submit" class="btn btn-danger">
+                        Logout
+                    </button>
+                </form>
             </div>
         </div>
     </div>
